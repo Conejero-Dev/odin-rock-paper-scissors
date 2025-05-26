89d0c15cd55d24d3a0262d95ed4ffd9877bc219f
Javier Fajardo <20287019+Conejero-Dev@users.noreply.github.com>
2025-05-26 20:41:37 +0100

    Add the logic to play a single round

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Rock, Paper or Scissors?").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    let human = humanChoice;
+    let computer = computerChoice;
+    if (
+        (human === "rock" && computer === "paper") ||
+        (human === "paper" && computer === "scissors") ||
+        (human === "scissors" && computer === "rock")
+    ) {
+        console.log(`You lose! ${computer} beats ${human}`);
+        computerScore++;
+    } else {
+        console.log(`You won! ${human} beats ${computer}`);
+        humanScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
