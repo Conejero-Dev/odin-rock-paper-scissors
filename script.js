5c8ca382116a500e7fa24ec6961faaaf453b07ea
Javier Fajardo <20287019+Conejero-Dev@users.noreply.github.com>
2025-05-26 20:34:30 +0100

    Add the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let idx = Math.floor(Math.random() * 3);
+    return options[idx];
+}
